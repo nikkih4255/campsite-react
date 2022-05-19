94987578eee708dfc3b8b27d416615c8db911af2
Nikki Hall <nikkih4255@gmail.com>
2022-05-19 13:10:49 -0500

    contact page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,6 @@
 import { Col, Row, Container, Card, CardBody, CardHeader } from "reactstrap";
 import SubHeader from "../components/SubHeader";
-import PartersList from "../features/partners/PartnersList";
+import PartnersList from "../features/partners/PartnersList";
 
 const AboutPage = () => {
   return (
@@ -61,7 +61,7 @@ const AboutPage = () => {
         <Col xs="12">
           <h3>Community Partners</h3>
         </Col>
-        <PartersList />
+        <PartnersList />
       </Row>
     </Container>
   );
