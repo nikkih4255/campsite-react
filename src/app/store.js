95c9c85f66d2deb7a4d1530296eb84026ab4610b
Nikki Hall <nikkih4255@gmail.com>
2022-05-28 13:30:42 -0500

    workshop 4

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import { campsitesReducer } from "../features/campsites/campsitesSlice";
+import { partnersReducer } from "../features/partners/partnersSlice";
+import { promotionsReducer } from "../features/promotions/promotionsSlice";
+import { commentsReducer } from "../features/comments/commentsSlice";
+import { userReducer } from "../features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    campsites: campsitesReducer,
+    partners: partnersReducer,
+    promotions: promotionsReducer,
+    comments: commentsReducer,
+    user: userReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger]),
 });
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,19 +1,17 @@
+import { useSelector } from "react-redux";
 import { Col, Row } from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from "./campsitesSlice";
 
 const CampsitesList = () => {
-  const campsites = selectAllCampsites();
+  const campsites = useSelector(selectAllCampsites);
+  console.log("campsites:", campsites);
 
   return (
     <Row className="ms-auto">
       {campsites.map((campsite) => {
         return (
-          <Col
-            md="5"
-            className="m-4"
-            key={campsite.id}
-          >
+          <Col md="5" className="m-4" key={campsite.id}>
             <CampsiteCard campsite={campsite} />
           </Col>
         );
